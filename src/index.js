77d365b963cead8935e6523b66172fe44cd65e4d
nata1ka <nata1ka.yurevich@gmail.com>
2024-10-12 21:05:30 +0300

    feat:solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const array = [];
+  const bracketsPair = bracketsConfig.reduce((acc, pair) => {
+    const [open, close] = pair;
+    acc[close] = open;
+    return acc;
+  }, {});
+  for (let i = 0; i < str.length; i++) {
+    const currentChar = str[i];
+    if (currentChar === '|' && array[array.length - 1] === '|') {
+      array.pop();
+    } else if (bracketsPair[currentChar] && array[array.length - 1] === bracketsPair[currentChar]) {
+      array.pop();
+    } else {
+      array.push(currentChar);
+    }
+  }
+  if (array.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
